1e8902f55d40d980469216e43826600af826f88c
cristianduque <cristian.duque@upr.edu>
2018-05-15 01:36:11 -0400

    added sin and exponential support

--- FourierTransform.py
+++ FourierTransform.py
@@ -1,6 +1,6 @@
 from scipy.fftpack import fft
 import matplotlib.pyplot as plt
-from numpy import cos,linspace,pi,abs
+from numpy import cos,linspace,pi,abs,sin,exp
 
 class FourierTransform(object):
    def __init__(self, equation, number_samples):
